c365315119a3b8bd87c375e37fb891369eed7180
Eduard Erja <eduard.erja@gmail.com>
2018-01-30 15:25:52 +0200

    Revisions urls.py

--- core/urls.py
+++ core/urls.py
@@ -6,13 +6,13 @@ from .views import Landing, Home, Explore, ExploreGroups, FollowingGroups, Expan
 urlpatterns = [
 
     url(r'^$', Landing.as_view(), name='index'),
+    
     url(r'^login/$', auth_views.login, name='login'),
 
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 
     url(r'^home/$', login_required(Home.as_view()), name='home'),
 
-    
     url(r'^explore/$', login_required(Explore.as_view()), name='explore'),
 
     url(r'^explore/groups/(?P<pk>\d+)$', ExploreGroups.as_view(), name='expand-category'),
@@ -21,7 +21,6 @@ urlpatterns = [
     
     url(r'^group/(?P<pk>\d+)$', login_required(ExpandGroup.as_view()), name='expand-group'),
     
-
     url(r'^post/(?P<pk>\d+)$', login_required(ExpandPost.as_view()), name='expand-post'),
 
     # url(r'^group/(?P<pk>\d+)$', login_required(FollowGroup.as_view()), name='follow'),
